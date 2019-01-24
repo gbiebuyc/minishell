a0f686e9fdfe7a524e97440e6c9b99206dfdc130
gbiebuyc <gbiebuyc@users.noreply.github.com>
2019-01-24 13:49:36 +0100

    Add builtin exit command

--- src/execute_line.c
+++ src/execute_line.c
@@ -6,13 +6,13 @@
 /*   By: gbiebuyc <gbiebuyc@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/23 23:14:56 by gbiebuyc          #+#    #+#             */
-/*   Updated: 2019/01/23 23:20:32 by gbiebuyc         ###   ########.fr       */
+/*   Updated: 2019/01/24 13:49:01 by gbiebuyc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	execute_line(char **args)
+void	launch_process(char **args)
 {
 	pid_t	is_father;
 
@@ -21,4 +21,19 @@ void	execute_line(char **args)
 		wait(NULL);
 	else
 		execve(args[0], args, NULL);
+}
+
+void	builtin_exit(char **args)
+{
+	if (args[1])
+		return (ft_putstr_fd("exit: too many arguments\n", 2));
+	exit(EXIT_SUCCESS);
+}
+
+void	execute_line(char **args)
+{
+	if (ft_strequ(args[0], "exit"))
+		builtin_exit(args);
+	else
+		launch_process(args);
 }
